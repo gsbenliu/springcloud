6b017e34afd19432e473159fcf0e0fa05944760e
3032898624678392 <liugang@fosunhealth.com>
2022-11-28 22:09:43 +0800

    初始化代码

--- springcloud-manager/src/main/java/com/example/springcloud/manager/OrderManager.java
+++ springcloud-manager/src/main/java/com/example/springcloud/manager/OrderManager.java
@@ -2,7 +2,6 @@ package com.example.springcloud.manager;
 
 import com.example.springcloud.dal.mapper.OrderMapper;
 import com.example.springcloud.dal.model.OrderDO;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
